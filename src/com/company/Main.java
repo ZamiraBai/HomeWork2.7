12eb1983db21b58d53dc009853c80a7f9cb7640d
ZamiraBai <baimurzaeva@hotmail.com>
2019-10-13 22:46:08 +0600

    Исправленный

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,56 +2,42 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<String> listA = new ArrayList<>(5);
         System.out.println("Список-А");
-        listA.add("A-1");
-        listA.add("A-2");
-        listA.add("A-3");
-        listA.add("A-4");
-        listA.add("A-5");
+        listA.add(scanner.nextLine());
+        listA.add(scanner.nextLine());
+        listA.add(scanner.nextLine());
+        listA.add(scanner.nextLine());
+        listA.add(scanner.nextLine());
+        System.out.println(listA);
 
-        Iterator<String> iterator = listA.iterator();
-        while (iterator.hasNext()) {
-            String e = iterator.next();
-            System.out.println(e);
-        }
-
+        listA.ensureCapacity(10);
         ArrayList<String> listB = new ArrayList<>(10);
         System.out.println("Список-Б");
-        listB.add("B-1");
-        listB.add("B-2");
-        listB.add("B-3");
-        listB.add("B-4");
-        listB.add("B-5");
-
-        iterator = listB.iterator();
-        while (iterator.hasNext()) {
-            String ee = iterator.next();
-            System.out.println(ee);
-        }
+        listB.add(scanner.nextLine());
+        listB.add(scanner.nextLine());
+        listB.add(scanner.nextLine());
+        listB.add(scanner.nextLine());
+        listB.add(scanner.nextLine());
+        System.out.println(listB);
 
         ArrayList<String> listC = new ArrayList<>();
-        listC.add("A-1");
-        listC.add("B-5");
-        listC.add("A-2");
-        listC.add("B-4");
-        listC.add("A-3");
-        listC.add("B-3");
-        listC.add("A-4");
-        listC.add("B-2");
-        listC.add("A-5");
-        listC.add("B-1");
-
+        System.out.println("Список-С");
+        Collections.sort(listA);
+        Collections.sort(listB);
+        Collections.reverse(listB);
+        listC.addAll(listA);
+        listC.addAll(listB);
         System.out.println(listC);
 
         Collections.sort(listC);
         listC.toString().length();
-
         System.out.println(listC);
     }
 }
